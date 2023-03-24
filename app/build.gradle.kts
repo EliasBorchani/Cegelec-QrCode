7f4171c0697ab2e814eb06bd8da8a6241360e7a6
Elias Borchani <elias.borchani@withings.com>
2023-03-24 22:15:00 +0100

    fixed some gradle issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,12 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.kotlinCompilerExtensionVersion.get()
+    }
 }
 
 dependencies {
